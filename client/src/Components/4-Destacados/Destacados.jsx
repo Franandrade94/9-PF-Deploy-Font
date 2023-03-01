114fd60dc34ea6f6ec23f19ca1d79a124942c954
Franandrade94 <franandrade15994@gmail.com>
2023-03-01 12:19:46 -0300

    Style Css

--- client/src/Components/4-Destacados/Destacados.jsx
+++ client/src/Components/4-Destacados/Destacados.jsx
@@ -38,6 +38,32 @@ class Destacados extends Component {
             slidesToScroll={3}
             autoplay={true}
             autoplaySpeed={2000}
+            responsive={[
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  initialSlide: 1
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]}
           >
             {products2?.map((product) => {
               return (
