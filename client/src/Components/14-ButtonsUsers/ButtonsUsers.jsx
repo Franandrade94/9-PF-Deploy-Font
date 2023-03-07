26ee7e4e5793e44e14e2fe4b80a69def6a4e4c3d
Franandrade94 <franandrade15994@gmail.com>
2023-03-07 18:35:13 -0300

    Button lOG in

--- client/src/Components/14-ButtonsUsers/ButtonsUsers.jsx
+++ client/src/Components/14-ButtonsUsers/ButtonsUsers.jsx
@@ -2,7 +2,6 @@ import "./buttonsusers.css";
 import React from "react";
 import ButtonLogIn from "./1-ButtonLogIn/ButtonLogIn";
 import { useAuth0 } from "@auth0/auth0-react";
-import img from "../../Images/user.png";
 import Profile from "../../Pages/5-Profile/Profile";
 import ButtonLogOut from "./2-ButtonLogOut/ButtonLogOut";
 
@@ -14,7 +13,6 @@ const ButtonsUsers = () => {
 
     return(
         <div className="ButtonUsersContainer">
-            <img alt="" src={img}/>
             {
                 isAuthenticated && JSON.parse(localStorage.getItem('user')) !== undefined ?
                     <>
