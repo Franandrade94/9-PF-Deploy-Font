95e88b58a40c79442815b83c1c5b256b96effce1
Franandrade94 <franandrade15994@gmail.com>
2023-03-09 09:38:23 -0300

    Borrado Logico

--- client/src/Components/5-Productcard/ProductCardContainer.jsx
+++ client/src/Components/5-Productcard/ProductCardContainer.jsx
@@ -22,27 +22,6 @@ class ProductCardContainer extends Component {
     this.props.getAllProducts(orderby, types, pricerange);
   }
 
-  /*
-  componentDidUpdate(prevProps) {
-    if (prevProps.products !== this.props.products) {
-      let products = this.props.products;
-      const products2 = products?.filter(product => product?.deleted === false);
-      console.log(products);
-      console.log(this.props);
-      this.setState({ products2: products2 }); // Actualizar el estado products2
-    }
-    // Verificar si hubo algún cambio en this.props.carrito
-    if (prevProps.carrito !== this.props.carrito) {
-      // Actualizar el estado local products2 con la propiedad carrito actualizada
-      const products2 = this.props.products?.filter(product => product?.deleted === false).map(product => {
-        const carrito = this.props.carrito.includes(product.id);
-        return { ...product, carrito };
-      });
-      this.setState({ products2 });
-    }
-  }
-  */
-
   componentWillUpdate(){
     console.log("aaaaa")
   }
@@ -57,14 +36,18 @@ class ProductCardContainer extends Component {
   };
   render() {
 
+    let products=[]
+        products = this.props.products
+        const products2 = products?.filter(product => product?.deleted === false);
+
     return (
       <div className="ProductCard-Container">
         <div>
           <div className="ProductCard-Home">
-            {this.props.products?.length === 0 ? (
+            { products2?.length === 0 ? (
               <Loading />
             ) : (
-              this.props.products?.map((product) => {
+              products2?.map((product) => {
                 return (
                   <div key={product.id}>
                     <ProductCard
